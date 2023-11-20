177936e119d522252f15fda05107e40f6ea36648
Duc Tai TRAN <tdts2010@gmail.com>
2023-11-20 16:50:16 +0100

    devise user

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+
+  has_many :mangas
+  has_many :bookings
+end
