e0c82235e814d687a53500ef460032bd17f4fed0
Harris Shahbaz <shahbazharris@gmail.com>
2023-11-21 16:13:01 +0100

    changed status type to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_100102) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_150954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_100102) do
     t.date "duration"
     t.bigint "user_id", null: false
     t.bigint "manga_id", null: false
-    t.boolean "status"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["manga_id"], name: "index_bookings_on_manga_id"
