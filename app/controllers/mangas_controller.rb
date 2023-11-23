b79d1b9238a61010ea064b9a93a7c57cbad21740
Iqbal Bashir <iqbal2bashir@gmail.com>
2023-11-23 12:27:45 +0100

    NoMoreFranglish

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -19,7 +19,7 @@ class MangasController < ApplicationController
     @manga = Manga.new(manga_params)
     @manga.user = current_user
     if @manga.save
-      redirect_to manga_path(@manga), notice: 'Manga mis en location avec succès.'
+      redirect_to manga_path(@manga), notice: 'Manga créée avec succès.'
     else
       render :new, status: :unprocessable_entity
     end
