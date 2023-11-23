f0c93e433107f9a7f43ba784cfa35bac9e72f0a7
Harris Shahbaz <shahbazharris@gmail.com>
2023-11-23 12:57:51 +0100

    carrousel on home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_141215) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_205701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_141215) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.float "price_per_day"
+    t.string "image_url"
     t.index ["user_id"], name: "index_mangas_on_user_id"
   end
 
