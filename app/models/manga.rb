914a2a1b61297ba920b28499052f5886fa4ed36a
Harris Shahbaz <shahbazharris@gmail.com>
2023-11-24 12:39:45 +0100

    search bar working

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -4,4 +4,11 @@ class Manga < ApplicationRecord
   has_many :bookings
   validates :title, :author, :category, presence: true
   validates :synopsis, presence: true, length: { minimum: 50 }
+
+  include PgSearch::Model
+  pg_search_scope :search_manga,
+    against: [ :title, :synopsis, :author, :category ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
