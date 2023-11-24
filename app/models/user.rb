e7a1ac5cd47aaddbdf160edd22d3acdb6baabfde
Harris Shahbaz <shahbazharris@gmail.com>
2023-11-24 14:51:29 +0100

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :avatarT
   validates :email, uniqueness: true
-
   has_many :mangas, through: :bookings
   has_many :bookings
 end
-
